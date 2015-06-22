c0df98dab04a15abb18adc877e54e65089e2d690
Alexey Kolmakov <alxkolm@bk.ru>
2015-06-22 23:25:11 +0300

    Insert metrics to DB

--- selftop.go
+++ selftop.go
@@ -11,6 +11,8 @@ import (
     "strings"
     "strconv"
     // "time"
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"
 )
 
 var sock mangos.Socket
@@ -44,12 +46,20 @@ type Counter struct {
 
 var prevEvent Event
 var counter Counter
+var db *sql.DB
+// Map windows to ID in DB
+var windows map[Window]int64
+var insertWindowCommand *sql.Stmt
+var selectWindowCommand *sql.Stmt
+var insertMetricsCommand *sql.Stmt
 
 func main() {
     var err error
     var msg []byte
 
 	createSocket()
+    
+    bootstrapData()
 
     for {
         if msg, err = sock.Recv(); err != nil {
@@ -115,7 +125,7 @@ func parseMessage(message string) (event Event) {
 
 func processEvent(event Event) {
     defer func(){prevEvent = event}()
-
+    windowId := processWindow(event.window)
     if prevEvent.time == 0 {
         return
     }
@@ -137,7 +147,12 @@ func processEvent(event Event) {
 
     if prevEvent.window != event.window {
         // TODO save record to DB for window from prevEvent
-
+        insertMetricsCommand.Exec(
+            windowId,
+            counter.time,
+            counter.motions,
+            counter.clicks,
+            counter.keys)
         // reset counter
         counter = Counter {
             motions: 0,
@@ -148,4 +163,50 @@ func processEvent(event Event) {
     }
     
     fmt.Printf("Window %s time %.3f sec motions: %d, clicks: %d, keys: %d\n", event.window.class, float32(counter.time)/1000, counter.motions, counter.clicks, counter.keys)
+}
+
+func processWindow(window Window) int64 {
+    if _, ok := windows[window]; !ok {
+        // try to find window in db
+        var id int64
+        err := selectWindowCommand.QueryRow(window.title, window.class).Scan(&id)
+        switch err {
+        case sql.ErrNoRows:
+            // store window to db
+            res, _ := insertWindowCommand.Exec(window.title, window.class)
+            id, _ = res.LastInsertId()
+            windows[window] = id
+        default:
+            windows[window] = id
+        }   
+    }
+    return windows[window]
+}
+
+func bootstrapData() {
+    windows = make(map[Window]int64)
+    var err error
+
+    db, err = sql.Open("mysql", "root@/selftop")
+    if err != nil {
+        panic("Could not create db connection")
+    }
+    
+    insertWindowCommand, err = db.Prepare(
+        "INSERT INTO window (title, class) VALUES (?,?)")
+    if err != nil {
+        panic("Could not create prepared statement." + err.Error())
+    }
+
+    selectWindowCommand, err = db.Prepare(
+        "SELECT id FROM window WHERE title = ? AND class = ? LIMIT 1")
+    if err != nil {
+        panic("Could not create prepared statement." + err.Error())
+    }
+
+    insertMetricsCommand, err = db.Prepare(
+        "INSERT INTO metrics SET window_id = ?, time = ?, motions = ?, clicks = ?, `keys` = ?")
+    if err != nil {
+        panic("Could not create prepared statement." + err.Error())
+    }
 }
